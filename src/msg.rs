c72d5025206de0af271852d60e89358d9da0b76a
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2023-05-25 23:50:23 -0700

    misc. updates up to lesson 8

---
+++ src/msg.rs
@@ -0,0 +1,36 @@
+use cosmwasm_std::Coin;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct InstantiateMsg {
+    #[serde(default)]
+    pub counter: u64,
+    pub minimal_donation: Coin,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Value {},
+    Incremented { value: u64 },
+
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecMsg {
+    Poke {},
+    Reset {
+        #[serde(default)]
+        counter: u64,
+    }
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct ValueResp {
+    pub value: u64,
+}
+
+//
